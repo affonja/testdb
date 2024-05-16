26b112b52c6353ebed8275c5928d2d3376f2d452
affonja <56819481+affonja@users.noreply.github.com>
2024-05-16 19:53:54 +0400

    1

--- public/index.php
+++ public/index.php
@@ -13,6 +13,19 @@ $conStr = sprintf(
 );
 
 $pdo = new \PDO($conStr);
-echo 'ok';
+$sql = 'CREATE TABLE IF NOT EXISTS urls (
+                id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                name varchar(255) UNIQUE NOT NULL
+                                );';
+$pdo->exec($sql);
+
+$sql = "insert into urls(name) VALUES ('ya.ru')";
+$this->pdo->exec($sql);
+
+$sql = "select * from urls";
+$rows = $pdo->query($sql)->fetchAll(\PDO::FETCH_ASSOC);
+
+echo 'ok<br>';
+print_r($rows);
 
 
